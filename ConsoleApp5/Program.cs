fd88b86fe120f3b1b302deb987c7b570507335c5
titas <titas@DESKTOP-K6MINIM>
2018-12-09 17:42:45 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var messenger= new Messenger();
+            var video = new Video {Title = "ojioij"};
+            var VideoEncoder = new VideoEncoder(video);
+            VideoEncoder.OnVideoEncoded += messenger.OnVideoEncoded;
+            VideoEncoder.Encode();
+
+        }
+
+        class Messenger
+        {
+            public void OnVideoEncoded(object sender, EventArgs e)
+            {
+                Console.WriteLine("Sending message...");
+            }
+        }
+
+        class VideoEncoder
+        {
+            public Video video { get; set; }
+            public event EventHandler OnVideoEncoded;
+
+            public VideoEncoder(Video video)
+            {
+                this.video = video;
+            }
+
+            public void Encode()
+            {
+                Console.WriteLine("Encoding...Video");
+                Thread.Sleep(2000);
+                OnVideoEncoded?.Invoke(this,EventArgs.Empty);
+                
+            }   
+        }
+    }
+
+    internal class Video
+    {
+        public string Title { get; set; }
+    }
+}
